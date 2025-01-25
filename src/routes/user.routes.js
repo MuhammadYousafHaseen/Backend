d2a6c6a70a4b337db7075c0a676ec8be776393f6
MuhammadYousafHaseen <muhammadyousaf71eb@gmail.com>
2025-01-25 20:45:15 +0500

    add subscription model

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -22,7 +22,8 @@ router.route("/login").post(loginUser);
 
 //secured Routes
 router.route("/logout").post( verifyJWT, logoutUser);
-router.route("/refresh-token").post(refreshAccessToken)
+router.route("/refresh-token").post(refreshAccessToken);
+
 
 
 export default router;
